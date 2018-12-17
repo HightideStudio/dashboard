cfb994f356dcbf505e582cc5376f81a4e0ba50b1
Matheus Ronfim <mronfim@gmail.com>
2018-12-17 02:07:33 -0500

    Styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,9 @@ class User < ApplicationRecord
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
+      user.username = auth.info.name
+      user.image_url = auth.extra.raw_info.profile_image_url
+      user.token = auth.credentials.token
     end
   end
 end
